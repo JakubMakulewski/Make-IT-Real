0db9576761ee8ea34ec2da778d2b5d9ec46e5eed
Daniel B <danielbagrowski590@gmail.com>
2024-12-07 16:44:00 +0100

    zmiany carditem (okienek z projektami). dodano tytuł, opis, przyciski.

--- frontend/src/components/CardItem.js
+++ frontend/src/components/CardItem.js
@@ -6,16 +6,24 @@ function CardItem(props) {
     return (
         <>
             <li className="cards__item">
+                {/*<button className="star_like"></button>*/}
                 <Link className="cards__item__link" to={props.path}>
-                    <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img 
-                            src={props.src} 
-                            alt="Travel Image" 
-                            className="cards__item__img" 
-                        />
-                    </figure>
-                    <div className="cards__item__info">
-                        <h5 className="cards__item__text">{props.text}</h5>
+                    {/*<figure className="cards__item__pic-wrap" data-category={props.label}>*/}
+                    {/*    <img*/}
+                    {/*        src={props.src}*/}
+                    {/*        alt="Travel Image"*/}
+                    {/*        className="cards__item__img"*/}
+                    {/*    />*/}
+                    {/*</figure>*/}
+                    <div className="project_card" data-category={props.label}>
+                        <h2 className="cards__item__text__title">{props.text}</h2>
+                        <p className="cards__item__text__description">{props.description}</p>
+                    </div>
+                    {/*<div className="cards__item__info">*/}
+                    <div className="cards__item__buttons">
+                        {/*<h5 className="cards__item__text">{props.text}</h5>*/}
+                        <button>Status</button>
+                        <button>Locked</button>
                     </div>
                 </Link>
             </li>
